eb8a9c8bccc9c2c6b964787de1f3d490e4170afa
Attila Dobi <attiladobi100@gmail.com>
2022-10-23 16:11:19 +0200

    sort and filter added

--- mern/client/src/components/recordList.js
+++ mern/client/src/components/recordList.js
@@ -23,6 +23,9 @@ const Record = (props) => (
  
 export default function RecordList() {
  const [records, setRecords] = useState([]);
+ const [sorted, setSorted] = useState("");
+ const [posFilter, setPosFilter] = useState("");
+ const [levFilter, setLevFilter] = useState("");
  
  // This method fetches the records from the database.
  useEffect(() => {
@@ -54,9 +57,33 @@ export default function RecordList() {
    setRecords(newRecords);
  }
  
+ function handleSort(property) {
+    return setSorted(property);
+ }
+
+ function handlePosFilter(property) {
+  return setPosFilter(property)
+}
+
+function handleLevFilter(property) {
+  return setLevFilter(property)
+}
+
+function filterPos(el){
+  if(posFilter===""){
+    return el
+  }else
+  return el.position.toLowerCase().includes(posFilter.toLowerCase())
+}
+function filterLev(el){
+  if(levFilter===""){
+    return el
+  }else
+  return el.level.toLowerCase().includes(levFilter.toLowerCase())
+}
  // This method will map out the records on the table
  function recordList() {
-   return records.map((record) => {
+   return records.filter(el => filterPos(el)).filter(el => filterLev(el)).sort((a,b) => (a[sorted] > b[sorted]) ? 1 : ((b[sorted] > a[sorted]) ? -1 : 0)).map((record) => {
      return (
        <Record
          record={record}
@@ -71,6 +98,19 @@ export default function RecordList() {
  return (
    <div>
      <h3>Record List</h3>
+     <label>Sort by:</label>
+      <select onChange={(e) => handleSort(e.target.value)} id="sort">
+        <option value=""></option>
+        <option value="firstName">First Name</option>
+        <option value="lastName">Last Name</option>
+        <option value="middleName">Middle Name</option>
+        <option value="position">Position</option>
+        <option value="level">Level</option>
+      </select>
+      <label>Filter by position:</label>
+      <input onChange={(e) => handlePosFilter(e.target.value)} type="text"></input>
+      <label>Filter by level:</label>
+      <input onChange={(e) => handleLevFilter(e.target.value)} type="text"></input>
      <table className="table table-striped" style={{ marginTop: 20 }}>
        <thead>
          <tr>
